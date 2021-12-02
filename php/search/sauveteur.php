11e7a5c89a6e89df7469533719ccb86818a4d4cc
igneefleur <igneefleur@gmail.com>
2021-12-02 23:43:40 +0100

    modification sauveteur.php

--- php/search/sauveteur.php
+++ php/search/sauveteur.php
@@ -3,11 +3,12 @@
 function recherche_sauveteur($database, $value){
   // on selectionne les colonnes des sauveteurs a partir d'un mot
   // contenu soit dans le prenom, soit dans le nom du sauveteur
-  $sauveteurs = $sauveteurs->query(''
+  $sauveteurs = $makeList($database, ''
   . 'SELECT `Sauveteur`.`id`, `Sauveteur`.`nom`, `Sauveteur`.`prenom` FROM `Sauveteur`'
   . "WHERE `Sauveteur`.`nom` LIKE '%$value%'"
   . "OR `Sauveteur`.`prenom` LIKE '%$value%'"
-  . 'LIMIT 50'
+  . 'LIMIT 50',
+  array('id', 'nom', 'prenom')
   );  // on limite le nombre de resultats a 50
 
   return $sauveteurs;
