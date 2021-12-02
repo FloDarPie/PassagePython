4d37ae62188cb59a2efc51ad4846cf6cc31c15ae
Me-k-01 <mecaman01@gmail.com>
2021-12-02 22:13:45 +0100

    ajout font awesome

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <!-- API -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" charset="utf-8"></script>
     <script src="http://semantic-ui.com/dist/semantic.min.js" charset="utf-8"></script>
+    <script src="https://kit.fontawesome.com/da84674338.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="http://semantic-ui.com/dist/semantic.min.css">
     <!-- Fichiers locaux -->
     <link rel="stylesheet" href="/css/master.css">
