066794106fb0d70e6edbf9f040af5cd1a9fd7c64
Me-k-01 <mecaman01@gmail.com>
2021-12-03 04:28:03 +0100

    bouton accessibilité

--- index.php
+++ index.php
@@ -26,13 +26,14 @@
   </head>
   <body>
     <header>
-        <h1><a href="index.php">Les Sauveteurs</a></h1>
-        <nav>
-          <ul>
-            <li><a href="#">page1</a></li>
-            <li><a href="#">page2</a></li>
-          </ul>
-        </nav>
+      <h1><a href="index.php">Les Sauveteurs</a></h1>
+      <nav>
+        <ul>
+          <li><a href="#">page1</a></li>
+          <li><a href="#">page2</a></li>
+        </ul>
+      </nav>
+      <button type="button" name="button"><i class="fas fa-wheelchair"></i></button>
     </header>
     <main>
       <banner>
