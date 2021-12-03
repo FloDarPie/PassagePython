b12cc08477916b0fb6308b5266bac6f008676f07
Enguerran-Couderc <enguerrancouderc@gmail.com>
2021-12-03 04:02:34 +0100

    popup handicap

---
+++ js/handicap.js
@@ -0,0 +1,17 @@
+document.getElementById('submit').addEventListener('click', submit);
+function submit() {
+    var values = [];
+    var cbs = document.forms['handi'].elements['handicap'];
+    for(var i=0,cbLen=cbs.length;i<cbLen;i++){
+        if(cbs[i].checked){
+            values.push(cbs[i].value);
+        } 
+    }
+    alert('You selected: ' + values.join(', '));
+}
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
