f287aa2ba598fe79d2bbeffea5407c24d24bd9c6
Me-k-01 <mecaman01@gmail.com>
2021-12-03 06:15:32 +0100

    plus de css creation element

--- php/pages/creation/bateau.php
+++ php/pages/creation/bateau.php
@@ -8,11 +8,14 @@
   </head>
   <body>
     <?php require "../../../php/header.php" ?>
-    <h1>CREATION DE BATEAU</h1>
-    <p>nom :</p>
-    <textarea id="name" name="nom" rows="1" cols="64"></textarea>
-    <p>description :</p>
-    <textarea id="resume" name="description" rows="32" cols="64"></textarea>
-    <button type="button" name="button" id="send">PUSH</button>
+    <main>
+      <h1>CREATION DE BATEAU</h1>
+      <p>nom :</p>
+      <textarea id="name" name="nom" rows="1" cols="64"></textarea>
+      <p>description :</p>
+      <textarea id="resume" name="description" rows="32" cols="64"></textarea>
+      <button type="button" name="button" id="send">PUSH</button>
+
+    </main>
   </body>
 </html>
--- php/pages/creation/sauve.php
+++ php/pages/creation/sauve.php
@@ -4,15 +4,20 @@
     <meta charset="utf-8">
     <title>Creation sauvé · Les Sauveteurs du Dunkerquois</title>
     <script src="/js/create/sauve.js" charset="utf-8" defer></script>
+    <?php require "../../../php/head.php" ?>
   </head>
   <body>
-    <h1>CREATION DE SAUVÉ</h1>
-    <p>nom :</p>
-    <textarea id="name" name="nom" rows="1" cols="64"></textarea>
-    <p>prenom :</p>
-    <textarea id="firstname" name="prenom" rows="1" cols="64"></textarea>
-    <p>description :</p>
-    <textarea id="resume" name="description" rows="32" cols="64"></textarea>
-    <button type="button" name="button" id="send">PUSH</button>
+    <?php require "../../../php/header.php" ?>
+    <main>
+      <h1>CREATION DE SAUVÉ</h1>
+      <p>nom :</p>
+      <textarea id="name" name="nom" rows="1" cols="64"></textarea>
+      <p>prenom :</p>
+      <textarea id="firstname" name="prenom" rows="1" cols="64"></textarea>
+      <p>description :</p>
+      <textarea id="resume" name="description" rows="32" cols="64"></textarea>
+      <button type="button" name="button" id="send">PUSH</button>
+
+    </main>
   </body>
 </html>
