be4ccb19975d5c2bb112fa21e32b1447297b529b
Me-k-01 <mecaman01@gmail.com>
2021-12-03 05:45:11 +0100

    head php

--- index.php
+++ index.php
@@ -3,19 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Les Sauveteurs du Dunkerquois</title>
-    <!-- API -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js" charset="utf-8"></script>
-    <script src="http://semantic-ui.com/dist/semantic.min.js" charset="utf-8"></script>
-    <script src="https://kit.fontawesome.com/da84674338.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="http://semantic-ui.com/dist/semantic.min.css">
-    <!-- Fichiers locaux -->
-    <link rel="stylesheet" href="css/master.css">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/ui.css">
-    <link rel="stylesheet" href="css/handicap.css">
-    <script src="js/search.js" charset="utf-8" defer></script>
-    <script src="js/handicap.js" defer></script>
-    <?php
+    <?php require "php/head.php";
       if(isset($_GET['dys']))
         echo '<link rel="stylesheet" href="css/dys.css">';
       if(isset($_GET['park']))
--- php/pages/view/sauveteur.php
+++ php/pages/view/sauveteur.php
@@ -3,12 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Vue sauveteur · Les Sauveteurs du Dunkerquois</title>
-    <link rel="stylesheet" href="/css/master.css">
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="stylesheet" href="/css/ui.css">
-    <link rel="stylesheet" href="/css/handicap.css">
-    <script src="/js/search.js" charset="utf-8" defer></script>
-    <script src="/js/handicap.js" defer></script>
+    <?php require "../../../php/head.php"; ?>
     <script src="/js/view/sauveteur.js" charset="utf-8" defer></script>
   </head>
   <body>
