2210493eb4767578a3bdab21fbdcb620dfc400b5
igneefleur <igneefleur@gmail.com>
2021-12-03 01:39:13 +0100

    ajout de bateau.php et sauve.php

---
+++ php/search/bateau.php
@@ -0,0 +1,19 @@
+<?php
+require "../connect.php";
+
+function recherche_bateau($database, $value){
+  // on selectionne les colonnes des sauveteurs a partir d'un mot
+  // contenu soit dans le prenom, soit dans le nom du bateau
+  $bateaux = makeList($database, ''
+  . 'SELECT `Bateau`.`id`, `Bateau`.`nom` FROM `Bateau`'
+  . "WHERE `Bateau`.`nom` LIKE '%$value%'"
+  . 'LIMIT 50',
+  array('id', 'nom')
+  );  // on limite le nombre de resultats a 50
+
+  return $bateaux;
+}
+
+echo recherche_bateau($db, $_POST['value']);
+
+?>
